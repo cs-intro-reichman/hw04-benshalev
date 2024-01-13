9b7946af88da3bfbe179c4366ddfebb664a90c2e
BenS <bshalev1@gmail.com>
2024-01-14 01:05:51 +0200

    home work

--- StringOps.java
+++ StringOps.java
@@ -21,13 +21,39 @@ public class StringOps {
     //////        in Recitation 3 question 5             ///////
     //////                                               ///////
     ////////////////////////////////////////////////////////////
+    
+    
     public static void main(String[] args) {
-        
+        String lowString = "Hello World";
+        System.out.println(capVowelsLowRest(lowString));
     }
 
-    public static String capVowelsLowRest (String string) {
+    public static String capVowelsLowRest (String str) {
         // Write your code here:
-        return "";
+        String fixstring = "";
+        int length = str.length() ;
+        for(int i = 0; i< length; i++){
+            char ch = str.charAt(i);
+            if (ch == ' '){
+                fixstring = fixstring + ch;
+            }
+            if ( ch >= 97 && ch <= 122){
+                if (ch == 97 || ch == 101 || ch == 111 || ch ==117 || ch == 105 ){
+                    ch = (char) (ch - 32);
+                    fixstring = fixstring + ch;
+                }
+            }
+            if (ch >= 65 && ch <= 90){
+                if (ch != 65 && ch != 69 && ch != 73 && ch != 79 && ch != 85){
+                    ch = (char) (ch + 32);
+                    fixstring = fixstring + ch;
+                }
+            }
+            else{
+                fixstring = fixstring + ch;
+            }
+        }
+        return fixstring;
     }
 
     public static String camelCase (String string) {
