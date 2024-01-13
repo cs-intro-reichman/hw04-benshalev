0d9326ae8a6bb537093267d4cd693c486520ea01
BenS <bshalev1@gmail.com>
2024-01-13 23:01:20 +0200

    home work

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,11 +1,24 @@
 public class ArrayOps {
     public static void main(String[] args) {
-        
+       int[] array1 = {0,1,2,3};
+       System.err.println(findMissingInt(array1)); 
     }
     
     public static int findMissingInt (int [] array) {
         // Write your code here:
-        return -1;
+        if (array.length == 1){
+            return 1;
+        }
+        int sum1 = 0;
+        int firstsum = array.length;
+        for(int i = 0; i < array.length; i++){
+            firstsum = firstsum + i;
+        }
+        for(int j = 0; j < array.length; j++ ){
+            sum1 = sum1 + array[j];
+        }
+        int ret = firstsum - sum1 ;  
+        return ret;
     }
 
     public static int secondMaxValue(int [] array) {
